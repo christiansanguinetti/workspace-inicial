9d9ba38265e618c3e00e215f41a0414d4186f9ad
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-08-10 22:53:30 -0300

    agregue funcion a products.js

---
+++ js/products.js
@@ -0,0 +1,30 @@
+const Autos_url = "https://japceibal.github.io/emercado-api/cats_products/101.json"
+
+function getJSONData(url) {
+    let result = {};
+    showSpinner()
+    return fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
+        })
+        .then(function (response) {
+            result.status = 'ok';
+            result.data = response;
+            hideSpinner()
+            return result;
+        })
+        .catch(function (error) {
+            result.status = 'error';
+            result.data = error;
+            hideSpinner();
+            return result;
+        });
+}
+ 
+for(producto of response.data.products){
+    console.log(...product);
+    }
