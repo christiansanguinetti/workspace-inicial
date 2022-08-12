5fd028f1f647c695611d225282dc0822ce8658b5
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-08-12 20:17:37 -0300

    html

--- js/login.js
+++ js/login.js
@@ -1,7 +1,14 @@
-function validar(){
+/*function validar(){
     var email = document.getElementById("floatingInput").value;
     var password = document.getElementById("floatingPassword").value;
     
     if (email && password !==0)
     window.location="principal.html";
+}*/
+function Login(){ 
+    var done=0; 
+    var usuario=document.login.usuario.value; 
+    var password=document.login.password.value; 
+    if (usuario=="USUARIO1" && password=="CONTRASEÑA1")  
+    window.location="principal.html"; 
 }
