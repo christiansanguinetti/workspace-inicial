c76d1ef65bc30910b232f84a0bfffb04a5de2a98
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-08-12 17:11:51 -0300

    agregue function nuevas

--- js/products.js
+++ js/products.js
@@ -1,3 +1,14 @@
 const Autos_url = "https://japceibal.github.io/emercado-api/cats_products/101.json"
- let productos = getJSONData(Autos_url);
- 
+
+document.addEventListener('DOMContentLoaded', async function () {
+    let contenedor_lista = document.getElementById('lista_productos');
+    const result = await getJSONData(Autos_url)
+
+    //vaciamos el <ul>..</ul>
+    contenedor_lista.innerHTML = "";
+    for (let producto of result.data.products) {
+        //agregamos un <li>.. </li> por cada producto que recorre el for()
+        contenedor_lista.innerHTML +=
+         `<li id='${ producto.id } '>${producto.name}</li>`;
+    }
+})
