5df654685d6788ef0688d33568c53c9fa4700e7f
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-08-11 22:45:04 -0300

    arreglos en index y productos

--- js/index.js
+++ js/index.js
@@ -1,17 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    for (const producto of response.data.products) {
-        console.log(producto);
-    }
     document.getElementById("autos").addEventListener("click", function () {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function () {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function () {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
+    localStorage.setItem("catID", 101);
+    window.location = "products.html"
+});
+document.getElementById("juguetes").addEventListener("click", function () {
+    localStorage.setItem("catID", 102);
+    window.location = "products.html"
+});
+document.getElementById("muebles").addEventListener("click", function () {
+    localStorage.setItem("catID", 103);
+    window.location = "products.html"
+});
 });
--- js/products.js
+++ js/products.js
@@ -1,32 +1,3 @@
 const Autos_url = "https://japceibal.github.io/emercado-api/cats_products/101.json"
-
-function getJSONData(url) {
-    let result = {};
-    showSpinner()
-    return fetch(url)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw Error(response.statusText);
-            }
-        })
-        .then(function (response) {
-            result.status = 'ok';
-            result.data = response;
-            hideSpinner()
-            return result;
-        })
-        .catch(function (error) {
-            result.status = 'error';
-            result.data = error;
-            hideSpinner();
-            return result;
-        });
-}
-
-function paraproducto() {
-    for (let producto of response.data.products) {
-        console.log(producto);
-    }
-}    
+ let productos = getJSONData(Autos_url);
+ 
