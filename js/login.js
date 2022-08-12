d68f104aeb8e27cdc7e56b4d8acc868bd94c4359
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-08-12 20:07:45 -0300

    asd

--- js/login.js
+++ js/login.js
@@ -1,27 +1,7 @@
-let emailcorrecto = false
-function validar(valor) {
-    re = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
-    if (!re.exec (valor)) {
-        alert('el email no es valido');
-        return emailcorrecto
-    } else {
-        emailcorrecto = true
-    }
+function validar(){
+    var email = document.getElementById("floatingInput").value;
+    var password = document.getElementById("floatingPassword").value;
+    
+    if (email && password !==0)
+    window.location="principal.html";
 }
-
-
-const formulario = document.getElementsByClassName("form-control")
-let boton = document.getElementById("button");
-boton.addEventListener("click", () => {
-let cantidadescompletas = 0;
-for (formulario of formularios) {
-
-    if (formulario.value.length != 0) {
-        cantidadescompletas += 1;
-    }
-}
-if (cantidadescompletas == 2 && emailcorrecto == true) {
-    location.href = "principal.html";
-}
-})
-
