feb631475ebbaecced7bb42d6b7c6a409c960d16
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-08-12 20:41:30 -0300

    agregue value

--- js/login.js
+++ js/login.js
@@ -1,14 +1,11 @@
-/*function validar(){
+function validar(){
     var email = document.getElementById("floatingInput").value;
     var password = document.getElementById("floatingPassword").value;
-    
-    if (email && password !==0)
-    window.location="principal.html";
-}*/
-function Login(){ 
-    var done=0; 
-    var usuario=document.login.usuario.value; 
-    var password=document.login.password.value; 
-    if (usuario=="USUARIO1" && password=="CONTRASEÑA1")  
-    window.location="principal.html"; 
+     
+    if (email && password !==0){
+        alert("email y password validos")
+        return window.location="principal.html"
+    }else{
+        alert("email y password invalidos")
+    }
 }
