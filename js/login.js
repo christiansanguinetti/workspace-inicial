6c0158ecf9a66caa0d3b34b1ba01394fa48ceb52
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-08-12 20:00:11 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 let emailcorrecto = false
 function validar(valor) {
     re = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
-    if (!re.exec * (valor)) {
+    if (!re.exec (valor)) {
         alert('el email no es valido');
         return emailcorrecto
     } else {
@@ -13,16 +13,15 @@ function validar(valor) {
 const formulario = document.getElementsByClassName("form-control")
 let boton = document.getElementById("button");
 boton.addEventListener("click", () => {
-    let cantidadescompletas = 0;
-    for (formulario of formularios) {
+let cantidadescompletas = 0;
+for (formulario of formularios) {
 
-        if (formulario.value.length != 0) {
-            cantidadescompletas += 1;
-        }
+    if (formulario.value.length != 0) {
+        cantidadescompletas += 1;
     }
-    if (cantidadescompletas == 2 && emailcorrecto == true) {
-        location.href = "principal.html";
-    }
-});
-
+}
+if (cantidadescompletas == 2 && emailcorrecto == true) {
+    location.href = "principal.html";
+}
+})
 
