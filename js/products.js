43c59b0ec559521019ab562b988cdf12b0a0611c
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-08-16 22:39:03 -0300

    agregue estructuras

--- js/products.js
+++ js/products.js
@@ -9,6 +9,21 @@ document.addEventListener('DOMContentLoaded', async function () {
     for (let producto of result.data.products) {
         //agregamos un <li>.. </li> por cada producto que recorre el for()
         contenedor_lista.innerHTML +=
-         `<li id='${ producto.id } '>${producto.name}</li>` ;
+        `
+        <div onclick="setCatID(${producto.id})" class="list-group-item list-group-item-action cursor-active">
+            <div class="row">
+                <div class="col-3">
+                    <img src="${producto.image}" alt="${producto.description}" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">${producto.name}</h4>
+                        <small class="text-muted">${producto.productCount} artículos</small>
+                    </div>
+                    <p class="mb-1">${producto.description}</p>
+                </div>
+            </div>
+        </div>
+        `
     }
 })
