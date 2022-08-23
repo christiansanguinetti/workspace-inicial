b158ee99fa0ac04b61252f77d51ece9d002a5b09
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-08-23 20:38:48 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,11 +5,12 @@ function validar(){
     if (usuario && password !==0){
         alert("usuario y password validos")
         return window.location="principal.html";
+        localStorage.setItem("usuario")
     }else{
         alert("usuario y password invalidos")
     }
 }
  
-function guardar_usuario(){
+/*function guardar_usuario(){
     if (usuario.value) localStorage.setItem("usuario", usuario.value);
-}
+}*/
