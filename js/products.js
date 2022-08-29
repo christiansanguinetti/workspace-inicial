cceba6c3fde9f9c869aeeed87b84dda1878347c5
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-08-29 19:55:25 -0300

    sume funciones

--- js/products.js
+++ js/products.js
@@ -1,5 +1,5 @@
-let catID =  localStorage.getItem("catID")
-const productos_url = "https://japceibal.github.io/emercado-api/cats_products/"+catID+".json"
+let catID = localStorage.getItem("catID")
+const productos_url = "https://japceibal.github.io/emercado-api/cats_products/" + catID + ".json"
 
 
 document.addEventListener('DOMContentLoaded', async function () {
@@ -8,9 +8,9 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     contenedor_lista.innerHTML = "";
     for (let producto of result.data.products) {
-        
+
         contenedor_lista.innerHTML +=
-        `
+            `
         <div onclick="setCatID(${producto.id})" class="list-group-item list-group-item-action cursor-active">
             <div class="row">
                 <div class="col-3">
@@ -29,3 +29,31 @@ document.addEventListener('DOMContentLoaded', async function () {
     }
 })
 
+const ordenar_desc = "-$";
+const ordenar_ascd = "+$";
+const ORDER_BY_PROD_COUNT = "Cant.";
+function Ordenar() {
+    let result = [];
+    if (criteria === ordenar_desc)
+    {        
+      result = producto.sort((a, b) => {
+        if (a.cost < b.cost) { return -1; }
+        if (a.cost > b.cost) { return 1; }
+        return 0;
+      })
+    } else if (criteria === ordenar_ascd){
+     result = producto.sort((a, b) => {
+        if (a.cost > b.cost) { return -1 }
+        if (a.cost < b.cost) { return 1 }
+        return 0;
+    })
+}
+}
+
+document.getElementById("sortAsc").addEventListener("click", function(){
+    sortAndShowCategories(ORDER_ASC_BY_NAME);
+});
+
+document.getElementById("sortDesc").addEventListener("click", function(){
+    sortAndShowCategories(ORDER_DESC_BY_NAME);
+});
