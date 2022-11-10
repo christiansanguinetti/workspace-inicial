54787decdb51fbe89302aac9adbd565fc86d5263
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-11-10 18:56:30 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,12 +1,20 @@
 
-function validar(){    
+function validar() {
     const usuario = document.getElementById("floatingInput").value;
-const password = document.getElementById("floatingPassword").value; 
-    if (usuario && password !==0){
+    const password = document.getElementById("floatingPassword").value;
+    if (usuario && password !== 0) {
         alert("usuario y password validos")
         localStorage.setItem("usuario", usuario)
-        return window.location="principal.html";
-    }else{
+        return window.location = "principal.html";
+    } else {
         alert("usuario y password invalidos")
     }
 }
+function borrar_localstorage() {
+    localStorage.removeItem('segundo_nombre');
+    localStorage.removeItem('nombre');
+    localStorage.removeItem('apellido');
+    localStorage.removeItem('segundo_apellido');
+    localStorage.removeItem('telefono');
+}
+borrar_localstorage()
